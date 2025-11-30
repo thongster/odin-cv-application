1e187f222cf72aadebf54b56c5aa6bff8d512540
thongster <106802301+thongster@users.noreply.github.com>
2025-11-30 10:10:46 +0700

    state for each general input and display on screen

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import "./App.css";
 import General from "./General";
 
 function App() {
-  const [count, setCount] = useState(0);
 
   return (
     <>
--- src/General.jsx
+++ src/General.jsx
@@ -1,22 +1,52 @@
+import { useState } from 'react'
+
 export default function General() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
 
-
-    return (
-        <>
-            <form action="/" method="post">
-                <div className="nameDiv">
-                    <label htmlFor="name">Name: </label>
-                    <input type="text" id="name" required placeholder="John D. Smith"/>
-                </div>
-                <div className="emailDiv">
-                    <label htmlFor="email">Email: </label>
-                    <input type="email" id="email" required placeholder="johndsmith@gmail.com"/>
-                </div>
-                <div className="phoneDiv">
-                    <label htmlFor="phone">Phone Number: </label>
-                    <input type="tel" id="phone" required placeholder="555-555-5555"/>
-                </div>
-            </form>
-        </>
-    )
+  return (
+    <>
+      <form action="/" method="post">
+        <div className="nameDiv">
+          <label htmlFor="name">Name: </label>
+          <input 
+          type="text" 
+          id="name" 
+          required 
+          placeholder="John D. Smith" 
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          />
+        </div>
+        <div className="emailDiv">
+          <label htmlFor="email">Email: </label>
+          <input
+            type="email"
+            id="email"
+            required
+            placeholder="johndsmith@gmail.com"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div className="phoneDiv">
+          <label htmlFor="phone">Phone Number: </label>
+          <input 
+            type="tel" 
+            id="phone" 
+            required 
+            placeholder="555-555-5555" 
+            value={phone}
+            onChange={(e) => setPhone(e.target.value)}
+          />
+        </div>
+      </form>
+      <div>
+        <p>{name}</p>
+        <p>{email}</p>
+        <p>{phone}</p>
+      </div>
+    </>
+  );
 }
