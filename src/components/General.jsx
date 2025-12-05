ffb3620ff4a96fb8a3dc5986b0303406b8916c26
thongster <106802301+thongster@users.noreply.github.com>
2025-12-06 00:19:26 +0700

    objective in GeneralDisplay with h2 if statement

--- src/components/General.jsx
+++ src/components/General.jsx
@@ -6,6 +6,7 @@ export default function General({
   setEmail,
   setPhone,
   setAddress,
+  setObjective,
   genEdit,
   setGenEdit,
   isGenEdit,
--- src/components/GeneralDisplay.jsx
+++ src/components/GeneralDisplay.jsx
@@ -3,6 +3,7 @@ export default function GeneralDisplay({
   email,
   phone,
   address,
+  objective,
   setGenEdit,
   setIsGenEdit,
   setTab,
@@ -12,7 +13,8 @@ export default function GeneralDisplay({
       name: name,
       email: email,
       phone: phone,
-      address: address
+      address: address,
+      objective: objective
     });
     setIsGenEdit(true); // flag to allow edit and autofill form with prev info
     setTab("General"); // switch to General tab
@@ -20,10 +22,13 @@ export default function GeneralDisplay({
 
   return (
     <div>
-      <h2>{name}</h2>
+      <h2>{name
+            ? name
+            : "General"}</h2>
       <p>{address}</p>
       <p>{email}</p>
       <p>{phone}</p>
+      <p className="objective">{objective}</p>
       <button onClick={handleEdit}>Edit</button>
     </div>
   );
